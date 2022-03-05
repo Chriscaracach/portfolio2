6525fdfc413eb8961c20ae3b264a9dc51aedb771
chriscaracach <christiancaracach@gmail.com>
2022-03-05 15:32:38 -0300

    algo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import Header from "./components/Header";
 import Grid from "./components/Grid";
 import Card from "./components/Card";
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,42 @@
 import React from "react";
 import TypeAnimation from "react-type-animation";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import EmailIcon from "@mui/icons-material/Email";
-import GitHubIcon from "@mui/icons-material/GitHub";
 
 const Header = () => {
   return (
-    <header className="container" id="header">
+    <header className="header">
       <div className="header__typingtext">
         <TypeAnimation
           cursor={false}
-          sequence={["Christian", 1000, "Caracach", 1000]}
+          sequence={[
+            "*Christian",
+            1000,
+            "*Caracach",
+            1000,
+            "*Frontend",
+            1000,
+            "*React",
+            1000,
+          ]}
           repeat={20}
           wrapper="h3"
         />
       </div>
       <div className="header__social">
-        <LinkedInIcon />
-        <EmailIcon />
-        <GitHubIcon />
+        <a href="mailto:christiancaracach@gmail.com" className="header__icon">
+          <i class="nes-icon gmail is-medium"></i>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/christian-caracach/"
+          className="header__icon"
+        >
+          <i class="nes-icon linkedin is-medium"></i>
+        </a>
+        <a href="https://github.com/Chriscaracach" className="header__icon">
+          <i class="nes-icon github is-medium"></i>
+        </a>
+        <a href="asd" className="header__icon">
+          {/* <ArticleIcon fontSize="large" /> */}
+        </a>
       </div>
     </header>
   );
